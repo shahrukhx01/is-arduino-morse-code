1db586816c15fe302b6cd65aa2ed3275f7554d51
shahrukhx01 <sk28671@gmail.com>
2021-05-21 17:04:50 +0200

    add webserver script

--- arduino/internet_connect/wifi_connection.ino/wifi_connection.ino.ino
+++ arduino/internet_connect/wifi_connection.ino/wifi_connection.ino.ino
@@ -1,162 +1,31 @@
-/*******************************************************************
-    A sample project for making a HTTP/HTTPS GET request on an ESP8266
-
-    It will connect to the given request and print the body to
-    serial monitor
-
-    Parts:
-    D1 Mini ESP8266 * - http://s.click.aliexpress.com/e/uzFUnIe
-
- *  * = Affilate
-
-    If you find what I do usefuland would like to support me,
-    please consider becoming a sponsor on Github
-    https://github.com/sponsors/witnessmenow/
-
-
-    Written by Brian Lough
-    YouTube: https://www.youtube.com/brianlough
-    Tindie: https://www.tindie.com/stores/brianlough/
-    Twitter: https://twitter.com/witnessmenow
- *******************************************************************/
-
-
-// ----------------------------
-// Standard Libraries
-// ----------------------------
-
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin("iPhone", "pass1234");
 
-//------- Replace the following! ------
-char ssid[] = "Vodafone-5C2C";       // your network SSID (name)
-char password[] = "yZkaLfhnna3rsEg4";  // your network key
-const int buttonPin = D5;
-// For Non-HTTPS requests
-// WiFiClient client;
-
-// For HTTPS requests
-WiFiClientSecure client;
-
-
-// Just the base of the URL you want to connect to
-#define TEST_HOST "129.159.252.59"
-
-// OPTIONAL - The finferprint of the site you want to connect to.
-#define TEST_HOST_FINGERPRINT "89 25 60 5D 50 44 FC C0 85 2B 98 D7 D3 66 52 28 68 4D E6 E2"
-// The finger print will change every few months.
-int buttonState = 1; 
-
-void setup() {
-
-  Serial.begin(9600);
- pinMode(buttonPin, INPUT);
-
- 
-
-  //--------
-
-  // If you don't need to check the fingerprint
-  // client.setInsecure();
-
-  // If you want to check the fingerprint
-  //client.setFingerprint(TEST_HOST_FINGERPRINT);
-
-  //makeHTTPRequest();
-}
-
-void makeHTTPRequest() {
-
-  // Opening connection to server (Use 80 as port if HTTP)
-  if (!client.connect(TEST_HOST, 443))
+  Serial.print("Connecting");
+  
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
-    Serial.println(F("Connection failed"));
-    return;
+    delay(500);
+    Serial.print(".");   
   }
 
-  // give the esp a breather
-  yield();
-
-  // Send HTTP request
-  client.print(F("GET "));
-  // This is the second half of a request (everything that comes after the base URL)
-  client.print("/api/v3/simple/price?ids=ethereum%2Cbitcoin&vs_currencies=usd%2Ceur"); // %2C == ,
-  client.println(F(" HTTP/1.1"));
-
-  //Headers
-  client.print(F("Host: "));
-  client.println(TEST_HOST);
-
-  client.println(F("Cache-Control: no-cache"));
-
-  if (client.println() == 0)
-  {
-    Serial.println(F("Failed to send request"));
-    return;
-  }
-  //delay(100);
-  // Check HTTP status
-  char status[32] = {0};
-  client.readBytesUntil('\r', status, sizeof(status));
-  if (strcmp(status, "HTTP/1.1 200 OK") != 0)
-  {
-    Serial.print(F("Unexpected response: "));
-    Serial.println(status);
-    return;
-  }
-
-  // Skip HTTP headers
-  char endOfHeaders[] = "\r\n\r\n";
-  if (!client.find(endOfHeaders))
-  {
-    Serial.println(F("Invalid response"));
-    return;
-  }
-
-  // This is probably not needed for most, but I had issues
-  // with the Tindie api where sometimes there were random
-  // characters coming back before the body of the response.
-  // This will cause no hard to leave it in
-  // peek() will look at the character, but not take it off the queue
-  while (client.available() && client.peek() != '{')
-  {
-    char c = 0;
-    client.readBytes(&c, 1);
-    Serial.print(c);
-    Serial.println("BAD");
-  }
-
-  // While the client is still availble read each
-  // byte and print to the serial monitor
-  while (client.available()) {
-    char c = 0;
-    client.readBytes(&c, 1);
-    Serial.print(c);
-  }
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  // put your main code here, to run repeatedly:
-  if (buttonState == HIGH) {
-      // Connect to the WiFI
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  // Attempt to connect to Wifi network:
-  Serial.println("Connecting Wifi: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println(WiFi.status());
-    Serial.print(".");
-    delay(500);
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+    WiFiClient client; //Declare an object of class WiFiClient
+    HTTPClient http;  //Declare an object of class HTTPClient
+    http.begin(client, "http://pkbilup6j6:pqh3u4t60h@pi-consult-neural-3709294104.eu-central-1.bonsaisearch.net/morse_code/_search");  //Specify request destination
+    int httpCode = http.GET();                                  //Send the request
+    if (httpCode > 0) { //Check the returning code
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);             //Print the response payload
+    }
+    http.end();   //Close connection
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
+  delay(30000);    //Send a request every 30 seconds
   }
-}
